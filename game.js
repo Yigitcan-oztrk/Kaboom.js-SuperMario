54b29f31af6046ccfa040395fd9a5ea2e4659239
DESKTOP-U2N87OI\Yigitcan <yigitcanozturk@ogr.bandirma.edu.tr>
2021-07-17 14:49:16 +0300

    update
    mario tuning

--- game.js
+++ game.js
@@ -82,27 +82,29 @@ scene("game", () => {
             update() {
                 if (isBig) {
                     CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
-                    timer -= dt()
-                    if (timer <= 0) {
+                    wait(timer, () => {
                         this.smallify
+                    });
 
-                    }
                 }
             },
             isBig() {
                 return isBig
             },
             smallify() {
-
+                
                 this.scale = vec2(1)
                 CURRENT_JUMP_FORCE = JUMP_FORCE
-                timer = 0
+                time = 0
                 isBig = false
+                
             },
             biggify(time) {
+                
                 this.scale = vec2(2)
                 timer = time
                 isBig = true
+                
             }
         }
     }
