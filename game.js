44eddd410956068b0d2568f406fd53d1b19754fd
DESKTOP-U2N87OI\Yigitcan <yigitcanozturk@ogr.bandirma.edu.tr>
2021-07-21 14:14:47 +0300

    mario size timer
    mario size identification

--- game.js
+++ game.js
@@ -79,33 +79,31 @@ scene("game", () => {
         let timer = 0
         let isBig = false
         return {
-          update() {
-            if (isBig) {
-              CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
-              timer -= dt()
-              if (timer <= 0) {
-                this.smallify()
-              }
+            update() {
+                if (isBig) {
+                    CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+                    timer -= dt()
+                    if (timer <= 0) {
+                        this.smallify()
+                    }
+                }
+            },
+            isBig() {
+                return isBig
+            },
+            smallify() {
+                this.scale = vec2(1)
+                CURRENT_JUMP_FORCE = JUMP_FORCE
+                timer = 0
+                isBig = false
+            },
+            biggify(time) {
+                this.scale = vec2(2)
+                timer = time
+                isBig = true
             }
-          },
-          isBig() {
-            return isBig
-          },
-          smallify() {
-            this.scale = vec2(1)
-            CURRENT_JUMP_FORCE = JUMP_FORCE
-            timer = 0
-            isBig = false
-          },
-          biggify(time) {
-            this.scale = vec2(2)
-            timer = time
-            isBig = true     
-          }
         }
-      }
-    
-
+    }
     const player = add([
         sprite('mario'), solid(),
         //haritada başlangıç pozisyonu belirlemek adına yazılan kod satırı
